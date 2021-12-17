dfac3b9b50e9f0e5611037e3c3bbeee9f702f8fc
Lloyd McFarlin <lmcfarlin@gmail.com>
2021-12-17 12:33:01 -0700

    Initial project skeleton

---
+++ Shared/MythologyChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  MythologyChessApp.swift
+//  Shared
+//
+//  Created by Lloyd McFarlin on 12/17/21.
+//
+
+import SwiftUI
+
+@main
+struct MythologyChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
